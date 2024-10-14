acdf813e0cb8f0440f626b8eec4434efb19e6a0e
Ben Phillips <ben@ben-phillips.net>
2024-10-14 19:14:13 +0100

    Change the indicator color to highlight when we're not on base

--- main.sh
+++ main.sh
@@ -12,4 +12,9 @@ fdRead=$REPLY
   layer=$(echo "$message" | /opt/homebrew/bin/jq -r '.LayerChange.new')
   printf "LayerChange: %s\n" "$layer"
   /opt/homebrew/bin/sketchybar --set keyboard-layer label="$layer"
+  if [ "$layer" = "base" ]; then
+    /opt/homebrew/bin/sketchybar --set keyboard-layer label.color="0xffffffff" background.color="0x00000000"
+  else
+    /opt/homebrew/bin/sketchybar --set keyboard-layer label.color="0xffffff00" background.color="0xffde3163"
+  fi
 done
