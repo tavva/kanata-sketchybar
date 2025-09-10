9bffc434f2f31485e48f81f9d72681a7909dd351
Ben Phillips <ben@ben-phillips.net>
2025-09-10 08:30:10 +0100

    Make the whole bar red when we're on a non-base layer

--- main.sh
+++ main.sh
@@ -11,10 +11,15 @@ fdRead=$REPLY
 /opt/homebrew/bin/rg 'LayerChange' --line-buffered <&$fdRead | while read -r message; do
   layer=$(echo "$message" | /opt/homebrew/bin/jq -r '.LayerChange.new')
   printf "LayerChange: %s\n" "$layer"
-  /opt/homebrew/bin/sketchybar --set keyboard-layer label="$layer"
   if [ "$layer" = "base" ]; then
-    /opt/homebrew/bin/sketchybar --set keyboard-layer label.color="0xffffffff" background.color="0x00000000"
+    /opt/homebrew/bin/sketchybar --set keyboard-layer label="$layer" label.color="0xffffffff" background.color="0x00000000"
+    /opt/homebrew/bin/sketchybar --bar color="0x00000000"
+    /opt/homebrew/bin/focusbar --set keyboard-layer label="$layer" label.color="0xffffffff" background.color="0x00000000"
+    /opt/homebrew/bin/focusbar --bar color="0x00000000"
   else
-    /opt/homebrew/bin/sketchybar --set keyboard-layer label.color="0xffffff00" background.color="0xffde3163"
+    /opt/homebrew/bin/sketchybar --set keyboard-layer label="$layer" label.color="0xffffff00" background.color="0xffde3163"
+    /opt/homebrew/bin/sketchybar --bar color="0x44de3163"
+    /opt/homebrew/bin/focusbar --set keyboard-layer label="$layer" label.color="0xffffff00" background.color="0xffde3163"
+    /opt/homebrew/bin/focusbar --bar color="0x44de3163"
   fi
 done
